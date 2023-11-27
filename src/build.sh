c79f79b2b3efeaea825d31a88cfae90135f01979
Benoit Baudaux <baudaux@yahoo.fr>
2023-11-28 00:11:51 +0100

    Remove build.sh

--- src/build.sh
+++
@@ -1,2 +0,0 @@
-/Users/baudauxbenoit/Documents/Dev/emscripten/emcc -c client.c -I ../wayland/src/ -I ../wayland/build/src/ -I ../wayland-protocols/stable/xdg-shell/ -I ../wayland-protocols/unstable/xdg-decoration -I ../../libxkbcommon/include/
-
